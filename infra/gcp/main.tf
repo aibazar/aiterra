c1a4710047fe85a9facf5032602a7166e82da31a
stefian <v_ianta@yahoo.com>
2020-08-20 12:21:00 -0300

    + provider "google" {

---
+++ infra/gcp/main.tf
@@ -0,0 +1,13 @@
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("terraform-key.json")
+
+  project = "aicolab"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+} 
