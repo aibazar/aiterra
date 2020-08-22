2ee9162233a369567ab8ede0339ee41bf1d8618f
stefian <v_ianta@yahoo.com>
2020-08-22 08:51:27 -0300

    + output "ip" {   value = google_compute_address.static_ip.address }

--- infra/gcp/main.tf
+++ infra/gcp/main.tf
@@ -12,10 +12,31 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
-terraform {
-  backend "gcs" {
-    bucket      = "terraform-ai"
-    prefix      = "terraform1"
-    credentials = "terraform-key.json"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
   }
 }
+
+resource "google_compute_address" "static_ip" {
+  name = "terraform-static-ip"
+}
+
+# terraform {
+#   backend "gcs" {
+#     bucket      = "terraform-ai"
+#     prefix      = "terraform1"
+#     credentials = "terraform-key.json"
+#   }
+# }
