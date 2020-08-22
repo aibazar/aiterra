61bad00b97e970c4cb0d4d787a89fb63a4a0c15c
stefian <v_ianta@yahoo.com>
2020-08-22 19:35:03 -0300

    + variable "cidrs" { default = "10.0.0.0/16" }

--- infra/gcp/variables.tf
+++ infra/gcp/variables.tf
@@ -4,4 +4,4 @@ variable "region" { default = "us-central1" }
 
 variable "zone" { default = "us-central1-c" }
 
-variable "cidr" { default = "10.0.0.0/16" }
+variable "cidrs" { default = "10.0.0.0/16" }
